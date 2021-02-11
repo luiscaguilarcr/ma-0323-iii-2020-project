04f03a1d750aefe2903534bc35d7a4d3cd48dded
Daniel Fonseca Solano <31803434+DanielFonsecaSolano@users.noreply.github.com>
2021-02-10 21:05:28 -0600

    Cambios

--- Métodos Numéricos.MATLAB/gauss_jordan.m
+++ Métodos Numéricos.MATLAB/gauss_jordan.m
@@ -1,35 +1,3 @@
-%___________________________________
-% Gauss Jordan Implementation
-% By Khaled Sharif
-%
-% Description:
-% This function will take a matrix designed to be used by the
-% Gauss-Jordan algorithm and solve it, returning a transposed
-% version   of the  last  column in the  ending  matrix which 
-% represents the solution to the unknown variables.
-%
-% Input:
-% The function  takes  one matrix of n by n+1, where n equals  
-% the number of unknown variables.  Each row  represents  the 
-% coefficients of the variables in each equation and the last
-% column  contains the  constants on the right  hand side  of
-% each equation.
-%
-% Output:
-% The  function  will    return a column vector  representing
-% the solution to each variable in order of appearance in the
-% given matrix. 
-%
-% Sample Input:
-% x = [1 3 1 10; 1 -2 -1 -6; 2 1 2 10];
-% gauss(x)
-%
-% Sample Output:
-% ans =
-%
-%    1.0000    2.0000    3.0000
-%
-%___________________________________
 
 function [y] = gauss_jordan (x)    
     for n = 1:(length(x)-1)
